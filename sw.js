55eacd426d851ac94b1c5d6b5e11b6050114171f
ACMAN1983 <90999867+ACMAN1983@users.noreply.github.com>
2026-04-06 20:39:37 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,11 @@
-const CACHE_NAME = 'sentinel-v855-cache';
+const CACHE_NAME = 'sentinel-v880-cache';
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['./', './index.html'])));
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['./', './index.html', './manifest.json'])));
+});
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.map((k) => k !== CACHE_NAME && caches.delete(k)))));
 });
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
