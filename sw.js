212a8d3213a1abadcd970d8a6f01eed5fec58ffd
ACMAN1983 <90999867+ACMAN1983@users.noreply.github.com>
2026-04-06 20:04:37 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'sentinel-v852-cache';
+const ASSETS = [
+  './',
+  './index.html',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
